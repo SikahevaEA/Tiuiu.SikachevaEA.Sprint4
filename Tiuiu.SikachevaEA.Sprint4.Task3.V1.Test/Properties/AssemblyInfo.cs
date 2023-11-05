0a62fb24e2df47c68288bcbda514737144132a70
Tiuiu.SikachovaEA@outlook.com <Tiuiu.SikachovaEA@outlook.com>
2023-11-05 22:37:45 +0500

    Task3

---
+++ Tiuiu.SikachevaEA.Sprint4.Task3.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tiuiu.SikachevaEA.Sprint4.Task3.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tiuiu.SikachevaEA.Sprint4.Task3.V1.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1379ba3b-7543-4d26-90fe-49f91db6f310")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tiuiu.SikachevaEA.Sprint4.Task3.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiuiu.SikachevaEA.Sprint4.Task3.V1.Lib;
+
+namespace Tiuiu.SikachevaEA.Sprint4.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas2 = new int[5, 5] { { 3, 4, 3, 4, 1 },
+                                          { 8, 1, 6, 6, 9 },
+                                          { 9, 8, 5, 1, 3 },
+                                          { 1, 8, 7, 1, 2 },
+                                          { 9, 9, 7, 5, 6 } };
+            
+            int rows = mas2.GetUpperBound(0) + 1;
+            int colums = mas2.Length / rows;
+
+
+            DataService ds = new DataService();
+           
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.WriteLine($"{mas2[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            int res = ds.Calculate(mas2);
+
+            Console.WriteLine("Сумма элементов последней строки массива = " + res);
+            Console.ReadKey();
+        }
+    }
+}
